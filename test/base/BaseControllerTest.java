0e2ef444a31cf69dc59a0c0c31b92978786ba504
Eduardo Aviles <eduardo.avilesj@gmail.com>
2017-02-16 20:39:13 -0600

    Separating test logic

---
+++ test/base/BaseControllerTest.java
@@ -0,0 +1,123 @@
+package base;
+
+import models.User;
+import play.Application;
+import play.ApplicationLoader.Context;
+import play.Environment;
+import play.db.Database;
+import play.db.Databases;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.guice.GuiceApplicationLoader;
+import play.test.Helpers;
+import repositories.JpaBadgeRepository;
+import repositories.JpaRegionRepository;
+import repositories.JpaTeamRepository;
+import repositories.JpaTokenRepository;
+import repositories.JpaUserRepository;
+import repositories.interfaces.BadgeRepository;
+import repositories.interfaces.RegionRepository;
+import repositories.interfaces.TeamRepository;
+import repositories.interfaces.TokenRepository;
+import repositories.interfaces.UserRepository;
+import services.BadgeServiceImpl;
+import services.RegionServiceImpl;
+import services.TeamServiceImpl;
+import services.UserAuthServiceImpl;
+import services.interfaces.BadgeService;
+import services.interfaces.RegionService;
+import services.interfaces.TeamService;
+import services.interfaces.UserAuthService;
+
+import javax.inject.Inject;
+
+import lombok.Getter;
+
+import org.junit.After;
+import org.junit.Before;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+
+/**
+ * Created by eduardo on 6/08/16.
+ */
+public class BaseRepositoryTest
+{
+
+  protected static final String USER_EMAIL = "user1@test.com";
+  protected static final String USER_PASSWORD = "PASSWORD1";
+
+  protected static final String USER_EMAIL_SIGNUP = "user2@test.com";
+  protected static final String USER_PASSWORD_SIGNUP = "PASSWORD2";
+  @Inject
+  protected UserAuthService userAuthService;
+
+  @Inject
+  Application application;
+  @Getter
+  Database database;
+
+  @Before
+  public void setup()
+  {
+    setupDatabase();
+    setupTestModules();
+    Helpers.start(this.application);
+    loadUserData();
+  }
+
+  private void setupDatabase()
+  {
+    this.database = Databases.inMemory(
+        "testDatabase",
+        ImmutableMap.of(
+            "MODE", "MYSQL"),
+        ImmutableMap.of(
+            "logStatements", true));
+  }
+
+  private void setupTestModules()
+  {
+
+    final GuiceApplicationBuilder builder = new GuiceApplicationLoader()
+        .builder(new Context(Environment.simple()))
+        .overrides(new AbstractModule()
+        {
+          @Override
+          protected void configure()
+          {
+            // Repositories
+            bind(UserRepository.class).to(JpaUserRepository.class).asEagerSingleton();
+            bind(TokenRepository.class).to(JpaTokenRepository.class).asEagerSingleton();
+            bind(TeamRepository.class).to(JpaTeamRepository.class).asEagerSingleton();
+            bind(BadgeRepository.class).to(JpaBadgeRepository.class);
+            bind(RegionRepository.class).to(JpaRegionRepository.class);
+            bind(Database.class).toInstance(getDatabase());
+            // Services
+            bind(UserAuthService.class).to(UserAuthServiceImpl.class);
+            bind(TeamService.class).to(TeamServiceImpl.class);
+            bind(BadgeService.class).to(BadgeServiceImpl.class);
+            bind(RegionService.class).to(RegionServiceImpl.class);
+          }
+        });
+    Guice.createInjector(builder.applicationModule()).injectMembers(this);
+
+  }
+
+  public void loadUserData()
+  {
+    final User user = new User();
+    user.setEmail(USER_EMAIL);
+    user.setPassword(USER_PASSWORD);
+    this.userAuthService.create(user);
+  }
+
+  @After
+  public void teardown()
+  {
+    getDatabase().shutdown();
+    Helpers.stop(this.application);
+
+  }
+}
---
+++ test/base/BaseTest.java
@@ -0,0 +1,8 @@
+package base;
+
+/**
+ * Created by eduardo on 16/02/17.
+ */
+public class BaseTest
+{
+}
